7cad538e7b0e28391d63bef799b2a763de984585
april sikes <aprilsikes@gmail.com>
2015-12-16 20:06:42 -0700

    more questions and answers

--- game/lib/questions.js
+++ game/lib/questions.js
@@ -1,10 +1,19 @@
 var questions = [
-  "Chandler was trapped in an ATM vestibule with what supermodel?",
-  "How did Joey originally get cast in his soap opera?",
-  "When Rachel and Phoebe go to get tattoos, what tattoo does Rachel choose?",
-  "Who does Rachel dress up as to satisfy a Ross fantasy?",
-  "What did Janice's first husband sell?",
-  "Where were Chandler and Monica when they decided to start a family?",
+  {"Question" : "Chandler was trapped in an ATM vestibule with what supermodel?", "Answer" : "Jill Goodacre"},
+
+  {"Question" : "How did Joey originally get cast in his soap opera?", "Answer" : "He slept with the casting director"},
+
+  {"Question" : "When Rachel and Phoebe go to get tattoos, what tattoo does Rachel choose?", "Answer" : "A heart"},
+
+  {"Question" : "Who does Rachel dress up as to satisfy a Ross fantasy?", "Answer" : "Princess Leia"},
+
+  {"Question" : "What did Janice's first husband sell?", "Answer" : "Mattresses"},
+
+  {"Question" : "Where were Chandler and Monica when they decided to start a family?", "Answer" : "In a hospital"},
+
+  {"Question" : "What holiday does Chandler despise?", "Answer" : "Thanksgiving"},
+
+  {"Question" : "In Las Vegas, what freak-show worthy discovery does Joey make?", "Answer" : "His identical hand twin"},
 
 ];
 
